71f3d1e3423ec730d09318d6998c766bfabcec05
Abel Cheng <abelcys@gmail.com>
2013-08-07 04:58:06 +0000

    V1.0.1.6 - Add sample code for using SqlLauncher and OracleLauncher

---
+++ DataAccess/SqlServer/DbExtensions.partial.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace DbParallel.DataAccess
+{
+	public static partial class DbExtensions
+	{
+		public static SqlBulkCopyColumnMapping Add(this SqlBulkCopyColumnMappingCollection columnMappings, string destinationColumn)
+		{
+			return columnMappings.Add(columnMappings.Count, destinationColumn);
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Copyright 2012 Abel Cheng
+//	This source code is subject to terms and conditions of the Apache License, Version 2.0.
+//	See http://www.apache.org/licenses/LICENSE-2.0.
+//	All other rights reserved.
+//	You must not remove this notice, or any other, from this software.
+//
+//	Original Author:	Abel Cheng <abelcys@gmail.com>
+//	Created Date:		2013-08-06
+//	Primary Host:		http://dbParallel.codeplex.com
+//	Change Log:
+//	Author				Date			Comment
+//
+//
+//
+//
+//	(Keep clean code rather than complicated code plus long comments.)
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ NuGet/Oracle/ODP.NET/DataAccess/ConfigHelper.sample.cs.pp
@@ -0,0 +1,70 @@
+﻿using System.Data.Common;
+using System.Configuration;
+
+namespace $rootnamespace$.DataAccess
+{
+	public static partial class ConfigHelper
+	{
+		#region Setting key names defined in your config file, can be overridden in partial OnInitializing()
+		private static string _ConnectionSettingKey = "$rootnamespace$.MainConnection";
+		private static string _PackageSettingKey = "$rootnamespace$.MainPackage";
+		private static string _AuxConnectionSettingKey = "$rootnamespace$.AuxConnection";
+		#endregion
+
+		#region Properties
+		private static DbProviderFactory _DbProviderFactory;
+		public static DbProviderFactory DbProviderFactory
+		{
+			get { return _DbProviderFactory; }
+		}
+
+		private static string _ConnectionString;
+		public static string ConnectionString
+		{
+			get { return _ConnectionString; }
+		}
+
+		private static string _DatabasePackage;
+		public static string DatabasePackage
+		{
+			get { return _DatabasePackage; }
+		}
+
+		private static DbProviderFactory _AuxDbProviderFactory;
+		public static DbProviderFactory AuxDbProviderFactory
+		{
+			get { return _AuxDbProviderFactory; }
+		}
+
+		private static string _AuxConnectionString;
+		public static string AuxConnectionString
+		{
+			get { return _AuxConnectionString; }
+		}
+		#endregion
+
+		static ConfigHelper()
+		{
+			OnInitializing();
+
+			#region Default Initialization
+			ConnectionStringSettings connSetting = ConfigurationManager.ConnectionStrings[_ConnectionSettingKey];
+			_DbProviderFactory = DbProviderFactories.GetFactory(connSetting.ProviderName);
+			_ConnectionString = connSetting.ConnectionString;
+
+			_DatabasePackage = ConfigurationManager.AppSettings[_PackageSettingKey];
+			if (_DatabasePackage == null)
+				_DatabasePackage = string.Empty;
+
+			connSetting = ConfigurationManager.ConnectionStrings[_AuxConnectionSettingKey];
+			_AuxDbProviderFactory = DbProviderFactories.GetFactory(connSetting.ProviderName);
+			_AuxConnectionString = connSetting.ConnectionString;
+			#endregion
+
+			OnInitialized();
+		}
+
+		static partial void OnInitializing();
+		static partial void OnInitialized();
+	}
+}
