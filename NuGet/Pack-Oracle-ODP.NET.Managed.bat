549e16ef8dd0798dc548534279db8f31e3503ddd
AbelCheng_cp <SND\AbelCheng_cp>
2013-08-16 06:10:31 +0000

    Reorganized NuGet packaging

--- NuGet/Pack-Oracle-ODP.NET.Managed.bat
+++ NuGet/Pack-Oracle-ODP.NET.Managed.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 COPY Oracle\ODP.NET.Managed\DataAccess.nuspec ..\DataAccess\DataAccess.nuspec /Y
-NuGet pack ..\DataAccess\DataAccess.csproj -Prop Configuration=Release
+..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -Prop Configuration=Release
 MOVE /Y *.nupkg nupkg
