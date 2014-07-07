00757eb77fd109563a4bc449b37da237b6b714e4
AbelCheng_cp <SND\AbelCheng_cp>
2014-07-07 19:33:10 +0000

    Fix NuGet command line build package issue on x64, by adding [-Prop Platform=AnyCPU]

--- NuGet/Pack-Oracle-ODP.NET.Managed.bat
+++ NuGet/Pack-Oracle-ODP.NET.Managed.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 CD /D %~dp0
 COPY Oracle\ODP.NET.Managed\DataAccess.nuspec ..\DataAccess\DataAccess.nuspec /Y
-..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -Prop Configuration=Release
+..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -Prop Configuration=Release;Platform=AnyCPU
 MOVE /Y *.nupkg nupkg
