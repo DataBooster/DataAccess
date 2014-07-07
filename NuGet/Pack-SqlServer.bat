4e82ee761691e41cf9e870085253b9e211fd1f7b
Abel Cheng <abelcys@gmail.com>
2014-07-07 19:33:10 +0000

    Fix NuGet command line build package issue on x64, by adding [-Prop Platform=AnyCPU]

--- NuGet/Pack-SqlServer.bat
+++ NuGet/Pack-SqlServer.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 CD /D %~dp0
 COPY SqlServer\DataAccess.nuspec ..\DataAccess\DataAccess.nuspec /y
-..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -Prop Configuration=Release
+..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -Prop Configuration=Release;Platform=AnyCPU
 MOVE /Y *.nupkg nupkg
