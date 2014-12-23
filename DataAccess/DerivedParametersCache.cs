98127790d4fce219b0f24dc24521e0b1029d752a
Abel Cheng <abelcys@gmail.com>
2014-12-23 22:08:13 +0000

    Preparing for DerivedParametersCache ..

--- DataAccess/DerivedParametersCache.cs
+++ DataAccess/DerivedParametersCache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.Common;
 using System.Collections.Generic;
 
@@ -22,12 +23,50 @@ namespace DbParallel.DataAccess
 			_CacheRoot = new Dictionary<string, StoredProcedureDictionary>(StringComparer.OrdinalIgnoreCase);
 		}
 
+		private static DbParameter[] GetCache(string connectionString, string storedProcedure)
+		{
+			StoredProcedureDictionary spDictionary;
+			DbParameter[] dbParameters = null;
+
+			lock (_DerivedParametersCacheLock)
+			{
+				if (_CacheRoot.TryGetValue(connectionString, out spDictionary))
+					spDictionary.TryGetValue(storedProcedure, out dbParameters);
+			}
+
+			return dbParameters;
+		}
+
+		private static void SetCache(string connectionString, string storedProcedure, DbParameter[] parameters)
+		{
+			StoredProcedureDictionary spDictionary;
+
+			lock (_DerivedParametersCacheLock)
+			{
+				if (_CacheRoot.TryGetValue(connectionString, out spDictionary) == false)
+				{
+					spDictionary = new StoredProcedureDictionary();
+					_CacheRoot.Add(connectionString, spDictionary);
+				}
+
+				spDictionary[storedProcedure] = parameters;
+			}
+		}
+
+		static private DbParameter[] RefreshParameters(DbCommand spCmd)
+		{
+			// TODO
+			return null;
+		}
+
 		static public bool DeriveParameters(DbCommand dbCommand, bool refresh)
 		{
 			if (dbCommand == null)
 				throw new ArgumentNullException("dbCommand");
 			if (dbCommand.Connection == null)
 				throw new ArgumentNullException("dbCommand.Connection");
+			if (dbCommand.CommandType != CommandType.StoredProcedure)
+				throw new ArgumentOutOfRangeException("dbCommand.CommandType", "Only supports CommandType.StoredProcedure!");
 
 			string connectionString = dbCommand.Connection.ConnectionString;
 			string storedProcedure = dbCommand.CommandText;
@@ -35,8 +74,17 @@ namespace DbParallel.DataAccess
 			if (string.IsNullOrWhiteSpace(storedProcedure))
 				throw new ArgumentNullException("dbCommand.CommandText");
 
+			DbParameter[] derivedParameters = null;
+
+			if (refresh || (derivedParameters = GetCache(connectionString, storedProcedure)) == null)
+			{
+				derivedParameters = RefreshParameters(dbCommand);
+				SetCache(connectionString, storedProcedure, derivedParameters);
+			}
+
 			// TODO
-			return true;
+
+			return (derivedParameters != null);
 		}
 	}
 }
