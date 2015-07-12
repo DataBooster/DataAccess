1bbaf99194a3dc74a63c394516b238fe982b9551
AbelCheng_cp <SND\AbelCheng_cp>
2015-07-12 04:50:18 +0000

    Changed Web/App.config.transform to Web/App.config.install/uninstall.xdt

--- NuGet/Pack-Oracle-ODP.NET.Managed.bat
+++ NuGet/Pack-Oracle-ODP.NET.Managed.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
 CD /D %~dp0
 COPY Oracle\ODP.NET.Managed\DataAccess.nuspec ..\DataAccess\DataAccess.nuspec /Y
-..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -Prop Configuration=Release;Platform=AnyCPU
-MOVE /Y *.nupkg nupkg
+..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -IncludeReferencedProjects -Symbols -Properties Configuration=Release;Platform=AnyCPU -OutputDirectory nupkg
