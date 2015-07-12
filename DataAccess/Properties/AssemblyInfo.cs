97401c82a509d41c16dc87f327cc79094bbff673
Abel Cheng <abelcys@gmail.com>
2015-07-12 04:50:18 +0000

    Changed Web/App.config.transform to Web/App.config.install/uninstall.xdt

--- DataAccess/Properties/AssemblyInfo.cs
+++ DataAccess/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DataBooster")]
-[assembly: AssemblyDescription("Less Code, Less Configuration, Less Time. DataBooster is a high-performance extension to ADO.NET Data Provider, includes a light encapsulation to minimize your DAL code and a booster to maximize throughput for writing mass data to database. Visit http://databooster.codeplex.com")]
+[assembly: AssemblyDescription("DataBooster is a high-performance extension to ADO.NET Data Provider, includes a light encapsulation to minimize your DAL code and a booster to maximize throughput for writing mass data to database. Visit http://databooster.codeplex.com")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Abel Cheng")]
 [assembly: AssemblyProduct("dbParallel Database Task Parallel Foundation")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.8.2")]
-[assembly: AssemblyFileVersion("1.7.8.2")]
+[assembly: AssemblyVersion("1.7.8.3")]
+[assembly: AssemblyFileVersion("1.7.8.3")]
--- NuGet/Pack-SqlServer.bat
+++ NuGet/Pack-SqlServer.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
 CD /D %~dp0
 COPY SqlServer\DataAccess.nuspec ..\DataAccess\DataAccess.nuspec /y
-..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -Prop Configuration=Release;Platform=AnyCPU
-MOVE /Y *.nupkg nupkg
+..\.nuget\NuGet.exe pack ..\DataAccess\DataAccess.csproj -IncludeReferencedProjects -Symbols -Properties Configuration=Release;Platform=AnyCPU -OutputDirectory nupkg
