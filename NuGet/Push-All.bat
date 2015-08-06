08d54d583aecbc41685253e384d5adc9c26ef1a0
Abel Cheng <abelcys@gmail.com>
2015-08-06 20:16:09 +0000

    Updated Push-All.bat

--- NuGet/Push-All.bat
+++ NuGet/Push-All.bat
@@ -5,27 +5,27 @@ IF /i {%1} == {} GOTO :Usage
 IF /i {%1} == {-h} GOTO :Usage
 IF /i {%1} == {-help} GOTO :Usage
 
-SET SQLPKG=nupkg\SqlServer\DataBooster.SqlServer.%1.nupkg
-SET OMGPKG=nupkg\Oracle.Managed\DataBooster.Oracle.Managed.%1.nupkg
-SET ODPPKG=nupkg\Oracle.ODP\DataBooster.Oracle.ODP.%1.nupkg
-SET DDTPKG=nupkg\Oracle.DataDirect\DataBooster.Oracle.DataDirect.%1.nupkg
-SET PREPARED=true
+SET PKGSQL=nupkg\SqlServer\DataBooster.SqlServer.%1.nupkg
+SET PKGOMG=nupkg\Oracle.Managed\DataBooster.Oracle.Managed.%1.nupkg
+SET PKGODP=nupkg\Oracle.ODP\DataBooster.Oracle.ODP.%1.nupkg
+SET PKGDDT=nupkg\Oracle.DataDirect\DataBooster.Oracle.DataDirect.%1.nupkg
+SET PKGPREPARED=true
 
-FOR %%P IN (%SQLPKG% %OMGPKG% %ODPPKG% %DDTPKG%) DO (
+FOR %%P IN (%PKGSQL% %PKGOMG% %PKGODP% %PKGDDT%) DO (
 IF NOT EXIST %%P (
 ECHO %%P does not exist!
-SET PREPARED=false
+SET PKGPREPARED=false
 )
 )
 
-IF {%PREPARED%} == {false} (
+IF {%PKGPREPARED%} == {false} (
 COLOR 0C
 PAUSE
 COLOR
 GOTO :EOF
 )
 
-FOR %%P IN (%SQLPKG% %OMGPKG% %ODPPKG% %DDTPKG%) DO ..\.nuget\NuGet.exe Push %%P
+FOR %%P IN (%PKGSQL% %PKGOMG% %PKGODP% %PKGDDT%) DO ..\.nuget\NuGet.exe Push %%P
 
 GOTO :EOF
 
@@ -36,5 +36,3 @@ ECHO     Push-All.bat version
 ECHO.
 ECHO Example:
 ECHO     Push-All.bat 1.7.8.8
-
-GOTO :EOF
