abd775061e27a55df034f36124743814776f88a0
AbelCheng_cp <SND\AbelCheng_cp>
2015-08-20 04:36:01 +0000

    Added abstract class DbContextBase

--- DataAccess/Properties/AssemblyInfo.cs
+++ DataAccess/Properties/AssemblyInfo.cs
@@ -25,12 +25,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.9.0")]
-[assembly: AssemblyFileVersion("1.7.9.0")]
+[assembly: AssemblyVersion("1.7.9.1")]
+[assembly: AssemblyFileVersion("1.7.9.1")]
