e88d28b72ea5b33c5aabe1abe2fae928c8672338
Abel Cheng <abelcys@gmail.com>
2015-10-10 03:45:21 +0000

    1.7.9.5

--- NuGet/Pack-StoredProcedureData.bat
+++ NuGet/Pack-StoredProcedureData.bat
@@ -3,4 +3,4 @@ CD /D %~dp0
 
 IF NOT EXIST nupkg\StoredProcedureData MKDIR nupkg\StoredProcedureData
 
-..\.nuget\NuGet.exe pack ..\DataAccess\Dynamic\StoredProcedureData\StoredProcedureData.csproj -IncludeReferencedProjects -Symbols -Properties Configuration=Release;Platform=AnyCPU -OutputDirectory nupkg\StoredProcedureData
+..\.nuget\NuGet.exe pack ..\DataAccess\Dynamic\ClientModels\StoredProcedureData.csproj -IncludeReferencedProjects -Symbols -Properties Configuration=Release;Platform=AnyCPU -OutputDirectory nupkg\StoredProcedureData
